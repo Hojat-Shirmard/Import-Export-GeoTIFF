90eda407f43a0bd3053d421acd73875b668b5101
Hojat Shirmard <30745811+Hojat-Shirmard@users.noreply.github.com>
2020-09-20 00:58:02 +0430

    Update Import_Export_Geotiff.py

--- Import_Export_Geotiff.py
+++ Import_Export_Geotiff.py
@@ -13,16 +13,18 @@ ds = gdal.Open(file)
 band = ds.GetRasterBand(1)
 arr = band.ReadAsArray()
 [cols, rows] = arr.shape
-arr_min = arr.min()
-arr_max = arr.max()
-arr_mean = int(arr.mean())
-arr_out = numpy.where((arr < arr_mean), 10000, arr)
+#arr_min = arr.min()
+#arr_max = arr.max()
+#arr_mean = int(arr.mean())
+#arr_out = numpy.where((arr < arr_mean), 10000, arr)
+arr2= arr.astype (float)
+arr_out = (arr2-np.min(arr2))/(np.max(arr2)-np.min(arr2)) # Normalization
 driver = gdal.GetDriverByName("GTiff")
 outdata = driver.Create("G:/Python_Learning/TEST/NB1.TIF", rows, cols, 1, gdal.GDT_UInt16) #for Landsat, gdal.GDT_UInt16 #for ASTER, gdal.GDT_Float64
 outdata.SetGeoTransform(ds.GetGeoTransform())##sets same geotransform as input
 outdata.SetProjection(ds.GetProjection())##sets same projection as input
 outdata.GetRasterBand(1).WriteArray(arr_out)
-outdata.GetRasterBand(1).SetNoDataValue(10000)##if you want these values transparent
+outdata.GetRasterBand(1)#.SetNoDataValue(10000)##if you want these values transparent
 outdata.FlushCache() ##saves to disk!!
 outdata = None
 band=None
